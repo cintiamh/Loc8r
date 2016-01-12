b49c2cfc0b055aa087ab57d8701bc199bc9e4c1b
Cintia Higashi <higashi@yahoo-inc.com>
2016-01-11 17:54:30 -0800

    Created listing page

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,5 @@
 module.exports.homeList = function(req, res) {
-    res.render('index', { title: 'Home' });
+    res.render('locations-list', { title: 'Home' });
 };
 
 module.exports.locationInfo = function(req, res) {
